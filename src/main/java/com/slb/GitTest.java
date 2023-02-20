4b5a4685309dbd9025b2a073319f20e61fc74a7b
slb <3068138122@qq.com>
2023-02-20 14:12:46 +0800

    idea first commit

---
+++ src/main/java/com/slb/GitTest.java
@@ -0,0 +1,12 @@
+package com.slb;
+
+/**
+ * @author:沈玲彬
+ * @create: 2023-02-20 14:10
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
